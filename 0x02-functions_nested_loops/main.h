12b95ba706bae9232c37cae992d5ccc191322239
thisislaait <ogadi.ugo@gmail.com>
2023-10-10 17:58:47 -0700

    main.h fully compiled

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,15 @@
 
 int _putchar(char c);
 int _islower(int c);
+int _isalphs(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
-
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
 #endif /* MAIN_H */
