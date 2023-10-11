e00e84cce503ca84890657969c35a4b2d0a0ea66
thisislaait <ogadi.ugo@gmail.com>
2023-10-10 18:44:13 -0700

    betty message added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,18 @@
 #include "6-abs.c"
 #include <stdio.h>
 
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ * @n: the starting number
+ *
+ * Description: This function prints all natural numbers from n to 98, separated
+ * by a comma and space, in ascending or descending order depending on the
+ * value of n. The first number printed is the value of n, and the last number
+ * is always 98.
+ */
+
+
+
 void print_to_98(int n)
 {
 	int i;
