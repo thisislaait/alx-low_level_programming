e04127a4eacb1f880672aad5cc797fdb03423a8d
thisislaait <ogadi.ugo@gmail.com>
2023-10-12 09:14:53 -0700

    betty also restored here, finally!

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,13 @@
  * main - testing positive or negative function on print
  * Return: 0
  */
+
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 0;
-    positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
