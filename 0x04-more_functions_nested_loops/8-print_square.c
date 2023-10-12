31bfc45f2baa56b1fd9758434bc6b711cea0a55f
thisislaait <ogadi.ugo@gmail.com>
2023-10-12 14:46:32 -0700

    double is always better

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - printsa new set of squares
+ *
+ * @size: integer for squares
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y< size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
