d88e4b5c704f3a40d1411cd54de7620a876815e8
thisislaait <ogadi.ugo@gmail.com>
2023-10-12 16:23:59 -0700

    it;s raining numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: character defined
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while  (divisor != 0)
+	{
+		_putchar(n / divisor + '0');
+		n % = divisor;
+		divisor / = 10;
+	}
+}
