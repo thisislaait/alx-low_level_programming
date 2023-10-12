a42fb669ba8fcaa1947cbcb6c222aaa77e3fd8f3
thisislaait <ogadi.ugo@gmail.com>
2023-10-12 15:05:53 -0700

    since I can do a FizzBuzz script, am I off the beginners list

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - numbers 0 to 100 with multiples of 3 , 5
+ *
+ *
+ * Return: a number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
