55aee8f68d9af60bcab842b8e6bc01e787babf0d
thisislaait <ogadi.ugo@gmail.com>
2023-10-12 13:19:29 -0700

    main.h updated

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif /* MAIN_H */
