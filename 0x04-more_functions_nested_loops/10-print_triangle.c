dec408b32e5f456d30b3eb9983b7bc91125ad9fe
thisislaait <ogadi.ugo@gmail.com>
2023-10-12 17:28:12 -0700

    betty spacing fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,13 +7,11 @@
 void print_triangle(int size)
 {
 	int i, j;
-	
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size - i; j++)
@@ -23,8 +21,7 @@ void print_triangle(int size)
 		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
-		}
-		
+		}	
 		_putchar('\n');
 	}
 }
