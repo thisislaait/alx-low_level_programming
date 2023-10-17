a907dfd9556b5ad93819effe2be606dbd0cd0a5b
thisislaait <ogadi.ugo@gmail.com>
2023-10-16 20:29:39 -0700

    compile errors fixed

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-    char password[65] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int sum = 2772;  /* Sum required by 101-crackme */
-    int rand_num, i;
-
-    srand(time(0));
-
-    while (sum > 122)
-    {
-        rand_num = (rand() % 62);
-        putchar(password[rand_num]);
-        sum -= (rand_num + 1);
-    }
-
-    putchar(sum);
-
-    return (0);
+	char password[65] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int sum = 2772;  /* Sum required by 101-crackme */
+	int rand_num;
+	
+	srand(time(0));
+	
+	while (sum > 122)
+	{
+		rand_num = (rand() % 62);
+		putchar(password[rand_num]);
+		sum -= (rand_num + 1);
+	}
+	
+	putchar(sum);
+	
+	return (0);
 }
