e0fe08cb60c65a665703730e6584cc786fb1e420
thisislaait <ogadi.ugo@gmail.com>
2023-10-16 19:54:19 -0700

    betty resolved

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,6 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
-	int digits_seen = 0;
 
 	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
 		i++;
@@ -27,11 +26,7 @@ int _atoi(char *s)
 	{
 		result = result * 10 + (s[i] - '0');
 		i++;
-		digits_seen++;
 	}
-
-	if (digit_seen == 0)
-		return (0);
 
 	return (result * sign);
 }
