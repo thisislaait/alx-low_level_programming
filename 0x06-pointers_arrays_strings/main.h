d78cc5f57a36cb4684d0b71be0e878da0f5e348c
thisislaait <ogadi.ugo@gmail.com>
2023-10-19 18:51:06 -0700

    files edited

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -38,6 +38,9 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+
 
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
@@ -46,7 +49,8 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 
