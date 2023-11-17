2bd15b345a6d703c186c5a5ded7cbc973ce27c63
thisislaait <ogadi.ugo@gmail.com>
2023-11-17 02:41:01 +0100

    debug: refactored function

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
- * free_list - Frees a list and its strings.
- * @head: Pointer to the head of the list.
+ * free_list - frees a list
+ * @head: head var
  */
-void free_list(list_t *head)
 void free_list(list_t *head)
 {
 	list_t *temporary;
