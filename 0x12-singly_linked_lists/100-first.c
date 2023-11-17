048cc1d4b140880039792b1a17fbe5526384ec3c
thisislaait <ogadi.ugo@gmail.com>
2023-11-17 03:00:45 +0100

    first updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 
 /**
- * printStatement - Print out a statement
+ * pre_main_message - Print out a statement
+ * Return: no return
  */
-void pre_main_message(void)
+void _attribute_((constructor)) pre_main_message()
 {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
-}
-
-/**
- * main - Entry point of the program
- * Return: Always 0
- */
-int main(void)
-{
-    // Call the function to print the message
-    print_main_message();
-
-    // Your main function code
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-
-    return 0;
 }
