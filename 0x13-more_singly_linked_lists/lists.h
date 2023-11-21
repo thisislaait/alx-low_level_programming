27fdea9cc2cc86f6e2025a51a91c3438d9579661
thisislaait <ogadi.ugo@gmail.com>
2023-11-21 12:11:06 -0800

    betty fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,9 +15,9 @@ int _putchar(char c);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /**
@@ -27,8 +27,8 @@ typedef struct list_s
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_list(const list_t *h);
