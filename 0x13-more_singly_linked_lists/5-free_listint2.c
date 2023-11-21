9fa87b4ec51470155739177a4bedd172d0e015c3
Ugo Laait <140851946+thisislaait@users.noreply.github.com>
2023-11-21 18:20:24 +0100

    Fix:Betty spacing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 /**
  * free_listint2 - frees a list and sets head to null
- * @head: Pointer to head of the list
+ * @head: Points to list head
  *
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+		listint_t *tmp;
 
-	if (head == NULL)
-		return;
-	while (*head != NULL)
-	{
-		tmp = *head;
-		free(*head);
-		*head = tmp->next;
-	}
-	*head = NULL;
+		if (head == NULL)
+			return;
+		while (*head != NULL)
+		{
+			tmp = *head;
+			free(*head);
+			*head = tmp->next;
+		}
+			*head = NULL;
 }
