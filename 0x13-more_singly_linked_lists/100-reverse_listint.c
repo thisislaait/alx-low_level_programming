c34ed015cc734de12e9b5c0e92f4d5707e8c4554
thisislaait <ogadi.ugo@gmail.com>
2023-11-21 18:13:30 +0100

    Modify: lists.h updated

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,20 +9,20 @@
   */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL, *next = NULL;
+	listint_t *p;
+	listint_t *n;
 
-	if (head)
+	p = NULL;
+	n = NULL;
+
+	while (*head != NULL)
 	{
-		while (*head)
-		{
-			next = *head;
-			*head = (*head)->next;
-			next->next = prev;
-			prev = next;
-		}
-
-		*head = prev;
-		return (*head);
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
 	}
-	return (NULL);
+
+	*head = p;
+	return (*head);
 }
