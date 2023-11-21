edbf794975682b6b54f1fffeb433f0c3a03aaf2a
thisislaait <ogadi.ugo@gmail.com>
2023-11-21 17:15:01 +0100

    Fix: betty lists.h Add: listint length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,33 +1,35 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 int _putchar(char c);
+
 /**
  * struct list_s - singly linked list
  * @str: string - (these are malloc string)
  * @len: length of the string
  * @next: points to the next node
- *
  */
 typedef struct list_s
 {
-	    char *str;
-	    unsigned int len;
-	    struct list_s *next;
-}       list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 /**
-* struct listint_s - more singly linked list
-* @n: integer use case
-* @next: pointing to next node
-*/
+ * struct listint_s - more singly linked list
+ * @n: integer use case
+ * @next: pointing to next node
+ */
 typedef struct listint_s
 {
-        int n;
-	    struct listint_s *next;
-}       listint_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -45,4 +47,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
