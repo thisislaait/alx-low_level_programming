f32fac32ac48f9e81188893c307855d1279636d7
thisislaait <ogadi.ugo@gmail.com>
2023-12-02 15:02:25 +0100

    Fix: betty

--- betty_happy.c
+++ betty_happy.c
@@ -11,8 +11,8 @@
  * @password: Array to store the password.
  */
 struct User {
-    char username[MAX_USERNAME_LEN];
-    char password[MAX_PASSWORD_LEN];
+	char username[MAX_USERNAME_LEN];
+	char password[MAX_PASSWORD_LEN];
 };
 
 /**
@@ -28,15 +28,19 @@ struct User users[MAX_USERS];
  *
  * Description: Registers a new user by storing their username and password.
  */
-void registerUser(const char *username, const char *password, int *numUsers) {
-    if (*numUsers < MAX_USERS) {
-        strcpy(users[*numUsers].username, username);
-        strcpy(users[*numUsers].password, password);
-        (*numUsers)++;
-        printf("User registered successfully!\n");
-    } else {
-        printf("Cannot register more users. User limit reached.\n");
-    }
+void registerUser(const char *username, const char *password, int *numUsers)
+{
+	if (*numUsers < MAX_USERS)
+	{
+		strcpy(users[*numUsers].username, username);
+		strcpy(users[*numUsers].password, password);
+		(*numUsers)++;
+		printf("User registered successfully!\n");
+	}
+	else
+	{
+		printf("Cannot register more users. User limit reached.\n");
+	}
 }
 
 /**
@@ -47,15 +51,18 @@ void registerUser(const char *username, const char *password, int *numUsers) {
  * Return: 1 if authentication is successful, 0 otherwise.
  *
  * Description: Authenticates a user by comparing input username and password
- *              with registered users' credentials.
+ * with registered users' credentials.
  */
-int authenticateUser(const char *username, const char *password, int numUsers) {
-    for (int i = 0; i < numUsers; ++i) {
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
-            return (1);  /* Authentication successful */
-        }
-    }
-    return (0);  /* Authentication failed */
+int authenticateUser(const char *username, const char *password, int numUsers)
+{
+	for (int i = 0; i < numUsers; ++i)
+	{
+		if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0)
+		{
+			return (1); /* Authentication successful */
+		}
+	}
+	return (0); /* Authentication failed */
 }
 
 /**
@@ -64,28 +71,32 @@ int authenticateUser(const char *username, const char *password, int numUsers) {
  *
  * Description: Simulates user registration and authentication.
  */
-int main() {
-    int numUsers = 0;
+int main(void)
+{
+	int numUsers = 0;
 
-    registerUser("user1", "password123", &numUsers);
+	registerUser("user1", "password123", &numUsers);
 
-    /* Simulate user login */
-    char inputUsername[MAX_USERNAME_LEN];
-    char inputPassword[MAX_PASSWORD_LEN];
+	/* Simulate user login */
+	char inputUsername[MAX_USERNAME_LEN];
+	char inputPassword[MAX_PASSWORD_LEN];
 
-    /* Get input from user */
-    printf("Enter username: ");
-    scanf("%s", inputUsername);
+	/* Get input from user */
+	printf("Enter username: ");
+	scanf("%s", inputUsername);
 
-    printf("Enter password: ");
-    scanf("%s", inputPassword);
+	printf("Enter password: ");
+	scanf("%s", inputPassword);
 
-    /* Authenticate the user */
-    if (authenticateUser(inputUsername, inputPassword, numUsers)) {
-        printf("Authentication successful. Access granted!\n");
-    } else {
-        printf("Authentication failed. Access denied.\n");
-    }
+	/* Authenticate the user */
+	if (authenticateUser(inputUsername, inputPassword, numUsers))
+	{
+		printf("Authentication successful. Access granted!\n");
+	}
+	else
+	{
+		printf("Authentication failed. Access denied.\n");
+	}
 
-    return (0);
+	return (0);
 }
