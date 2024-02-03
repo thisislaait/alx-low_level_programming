5a07d4d49b9c9a9c609e3c4086b8a7c281f7d002
thisislaait <ogadi.ugo@gmail.com>
2024-02-03 03:15:06 +0100

    Add: hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,24 +6,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *newTable;
-	unsigned int i;
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (size > 0)
-	{
-		newTable = malloc(sizeof(hash_table_t));
-		if (!newTable)
-			return (NULL);
-		newTable->size = size;
-		newTable->array = malloc(sizeof(hash_node_t *) * size);
-		if (!newTable->array)
-		{
-			free(newTable);
-			return (NULL);
-		}
-		for (i = 0; i < size; i++)
-			newTable->array[i] = NULL;
-		return (newTable);
-	}
-	return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
